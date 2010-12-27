fdddf208af2ebd24c49495402addbafa04e3607b
Doug Alcorn <dougalcorn@gmail.com>
2010-12-27 16:11:11 -0500

    rebind my function keys for main stuff to make chris more happy

--- fkeys.el
+++ fkeys.el
@@ -6,11 +6,14 @@
 (global-set-key [f2] 'eshell)
 (global-set-key (kbd "M-<f2>") '(lambda () (interactive) (eshell t)))
 (global-set-key [(control f2)] 'shell-command)
-(global-set-key [f5] 'compile)
 (global-set-key (kbd "M-r") 'ruby-rake)
-(global-set-key (kbd "A-M-<f8>") 'ruby-test-file)
+(global-set-key (kbd "M-<f8>") 'compile) ; this is for "re-compile"
+(global-set-key (kbd "<f8>") 'ruby-test-file)
 (global-set-key (kbd "A-<f8>") 'ruby-test-function)
 (global-set-key [f7] 'toggle-ruby-compilation-mode)
 
 (global-set-key [f9] 'rgrep)
 (global-set-key [f10] 'magit-status)
+
+(global-set-key (kbd "A-n") 'find-file-in-tags)
+(global-set-key (kbd "A-M-l") 'indent-region)
