42c94759e8203e27465775dc110e1c832491c502
Doug Alcorn <dougalcorn@gmail.com>
2010-12-27 16:07:08 -0500

    setting up both nxml and nxhtml modes

--- nxml-config.el
+++ nxml-config.el
@@ -1,3 +1,5 @@
+(require 'nxml-mode)
+
 (defun dka-nxml-fix-dabbrev ()
   (interactive)
   (make-local-variable 'dabbrev-abbrev-char-regexp)
@@ -6,3 +8,11 @@
   (setq dabbrev-abbrev-char-regexp "\\sw"))
 
 (add-hook 'nxml-mode-hook 'dka-nxml-fix-dabbrev)
+
+(require 'rng-loc)
+
+(add-to-list 'rng-schema-locating-files (concat my-config-dir "schemas/schemas.xml"))
+
+(mapcar '(lambda (binding) (add-to-list 'auto-mode-alist binding))
+        '(("\\.xml" . nxml-mode)
+          ("\\.xsd" . nxml-mode)))
