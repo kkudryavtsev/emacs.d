ce001d0d504e2a43c06be8082d9e6ba68bc29eb1
Doug Alcorn <dougalcorn@gmail.com>
2011-03-29 10:58:39 -0400

    updating my nxml schemas to include the html5 stuff

--- nxhtml-config.el
+++ nxhtml-config.el
@@ -1,6 +1,6 @@
 (load (concat my-config-dir "site-lisp/nxhtml/nxhtml/autostart.el"))
 
-(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . eruby-nxhtml-mumamo))
+(add-to-list 'auto-mode-alist '("\\.html\\.erb" . eruby-nxhtml-mumamo))
 
 (eval-after-load "rng-loc"
   '(add-to-list 'rng-schema-locating-files "~/code/html5-el/schemas.xml"))
